3b0f7f35394e3e0818b07dbde5d891604927e50c
roa5108 <108443489+roa5108@users.noreply.github.com>
2025-01-05 00:44:14 +0900

    [Silver V] Title: 영화감독 숌, Time: 548 ms, Memory: 23244 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -0,0 +1,9 @@
+let n = Number(require("fs").readFileSync("/dev/stdin").toString().trim());
+let num = 666;
+let count = 1;
+while (count != n) {
+    num++;
+    if (String(num).includes("666")) count++;
+}
+
+console.log(num);
