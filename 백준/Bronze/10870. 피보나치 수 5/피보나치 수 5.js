0907127cd05b4b7efec3efcf2e555ce39b78585e
roa5108 <108443489+roa5108@users.noreply.github.com>
2025-01-05 00:43:23 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 172 ms, Memory: 9676 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const n = fs.readFileSync("/dev/stdin");
+function fib(n){
+    if (n==0){
+        return 0;
+    }
+    else if (n==1){
+        return 1;
+    }
+    else{
+        return fib(n-1)+fib(n-2);
+    }
+}
+console.log(fib(n));
