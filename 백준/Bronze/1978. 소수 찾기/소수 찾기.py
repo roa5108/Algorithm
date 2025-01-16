aa10c2088ab353810700d678ccab6c4cf39ad815
roa5108 <108443489+roa5108@users.noreply.github.com>
2025-01-16 17:38:06 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+num=list(map(int,input().split()))
+cnt=0
+
+for x in num:
+    for i in range(2,x+1):
+        if x%i==0:
+            if x==i:
+                cnt+=1
+            break
+print(cnt)
