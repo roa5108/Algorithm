a16dccb2208cc88fbd23ecd7e332ba108c30b4f1
Roa Kim <roa5108@gmail.com>
2025-01-20 22:35:44 +0900

    [Silver IV] Title: 스택 2, Time: 796 ms, Memory: 88788 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -6,24 +6,24 @@ n = int(input().strip())
 stack = []
 
 for i in range(n):
-    command = list(map(int, input().strip().split()))
+    command = input().strip().split()
 
-    if command[0] == 1:
+    if command[0] == "1":
         stack.append(command[1])
 
-    elif command[0] == 2:
+    elif command[0] == "2":
         if not stack:
             print(-1)
         else:
             print(stack.pop())
 
-    elif command[0] == 3:
+    elif command[0] == "3":
         print(len(stack))
 
-    elif command[0] == 4:
+    elif command[0] == "4":
         print(1 if not stack else 0)
 
-    elif command[0] == 5:
+    elif command[0] == "5":
         if not stack:
             print(-1)
         else:
