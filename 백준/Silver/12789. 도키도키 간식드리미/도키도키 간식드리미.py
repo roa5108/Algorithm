8710a8760ac5a9738406f8d3e5765e681d560493
Roa Kim <roa5108@gmail.com>
2025-01-21 16:02:02 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -5,25 +5,16 @@ input = sys.stdin.readline
 n = int(input())
 people = list(map(int, input().split()))
 stack = []
-line = []
 current = 1
 
 for person in people:
+    stack.append(person)
+
     while stack and stack[-1] == current:
-        line.append(stack.pop())
+        stack.pop()
         current += 1
 
-    if person == current:
-        line.append(person)
-        current += 1
-    else:
-        stack.append(person)
-
-while stack and stack[-1] == current:
-    line.append(stack.pop())
-    current += 1
-
-if line == sorted(people):
+if not stack:
     print("Nice")
 else:
     print("Sad")
