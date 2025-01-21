0bda33ee6a4bdbaaf2845fe7896effe62f433b66
Roa Kim <roa5108@gmail.com>
2025-01-21 17:47:32 +0900

    [Silver III] Title: queuestack, Time: 196 ms, Memory: 49896 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+m = int(input())
+c = list(map(int, input().split()))
+
+result = deque()
+
+for i in range(n):
+    if a[i] == 0:
+        result.appendleft(b[i])
+
+for j in range(m):
+    result.append(c[j])
+    print(result.popleft(), end=" ")
