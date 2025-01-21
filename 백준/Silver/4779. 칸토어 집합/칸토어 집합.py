a6fbe747d1ae57f41c256350925f3aab86244f86
Roa Kim <roa5108@gmail.com>
2025-01-21 21:21:33 +0900

    [Silver III] Title: 칸토어 집합, Time: 36 ms, Memory: 33796 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,17 @@
+def solve(n):
+    if n == 1:
+        return "-"
+
+    left = solve(n // 3)
+    center = " " * (n // 3)
+    return left + center + left
+
+
+while True:
+    try:
+        N = int(input())
+
+        rst = solve(3**N)
+        print(rst)
+    except:
+        break
