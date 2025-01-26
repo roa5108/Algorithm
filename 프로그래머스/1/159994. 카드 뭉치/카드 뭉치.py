c04ef09e7a5e09732972daabd2e36b15b097ff21
Roa Kim <roa5108@gmail.com>
2025-01-27 01:42:34 +0900

    Update 카드 뭉치.py

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -2,15 +2,12 @@ def solution(cards1, cards2, goal):
     from collections import deque
     cards1=deque(cards1)
     cards2=deque(cards2)
-    tmp=[]
     
     for word in goal:
         if cards1 and cards1[0]==word:
-            tmp.append(word)
             cards1.popleft()
             
         elif cards2 and cards2[0]==word:
-            tmp.append(word)
             cards2.popleft()
             
         else:
