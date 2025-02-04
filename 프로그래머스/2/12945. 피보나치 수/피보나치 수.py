f1ddfd2923b64e6c8d0c430bb221ae8602b67a77
Roa Kim <roa5108@gmail.com>
2025-02-05 02:02:35 +0900

    [level 2] Title: 피보나치 수, Time: 88.71 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,18 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+def solution(n):
+    return fibo(n)%1234567
+
+
+def fibo(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    a,b=0,1
+    for i in range(2,n+1):
+        a,b=b,a+b
+        
+    return b
+        
