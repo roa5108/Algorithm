726d06e964354f9ab6b5b2d7b2f199ae8fddc3ef
Roa Kim <roa5108@gmail.com>
2025-03-08 17:17:51 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 480 ms, Memory: 69288 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -1,32 +1,34 @@
 import sys
-input=sys.stdin.readline
+
+input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
-N,M,R=map(int,input().split())
+N, M, R = map(int, input().split())
 
-visited=[False]*(N+1)
-graph={i:[] for i in range(1,N+1)}
-ans=[0]*(N+1)
+visited = [0] * (N + 1)
+graph = [[] for _ in range(N + 1)]
 
 
 for _ in range(M):
-    a,b=map(int,input().split())
+    a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-for key in graph:
+for key in range(1, N + 1):
     graph[key].sort(reverse=True)
-    
-order=1
+
+
+cnt = 1
+
 
 def dfs(idx):
-    global order
-    visited[idx]=True
-    ans[idx]=order
+    global cnt
+    visited[idx] = cnt
     for i in graph[idx]:
         if not visited[i]:
-            order+=1
+            cnt += 1
             dfs(i)
-            
+
+
 dfs(R)
-print('\n'.join(map(str,ans[1:])))
+print("\n".join(map(str, visited[1:])))
