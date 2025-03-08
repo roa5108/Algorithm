a672c85d103716929c6b83825d3250f2ef4d2ea9
Roa Kim <roa5108@gmail.com>
2025-03-08 19:46:48 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+# ladder = [list(map(int, input().split())) for _ in range(N)]
+# snake = [list(map(int, input().split())) for _ in range(M)]
+# max_box = max(ladder, key=lambda x: x[1])
+
+ladder = {}
+snake = {}
+visited = [0] * 101
+
+for _ in range(N):
+    a, b = map(int, input().split())
+    ladder[a] = b
+
+for _ in range(M):
+    c, d = map(int, input().split())
+    snake[c] = d
+
+
+def bfs(idx):
+    visited[idx] = 1
+    q = deque([idx])
+    while q:
+        now = q.popleft()
+        for dice in range(1, 7):
+            next = now + dice
+            if next == 100:
+                return visited[now]
+            if next in ladder:
+                next = ladder[next]
+            if next in snake:
+                next = snake[next]
+            if next <= 100 and visited[next] == 0:
+                visited[next] = visited[now] + 1
+                q.append(next)
+
+
+print(bfs(1))
