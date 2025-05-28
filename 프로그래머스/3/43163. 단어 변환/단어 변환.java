86537c31817b6f931fc5b39b1d1c0062c0345c78
Roa Kim <roa5108@gmail.com>
2025-05-29 00:07:40 +0900

    [level 3] Title: 단어 변환, Time: 0.40 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,42 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        Queue<String> queue = new ArrayDeque<>();
-        queue.add(begin);
-        Map<String, Boolean> visited = new HashMap<>();
-        int depth =0; //몇단계 거쳤는지
-        visited.put(begin,true); //begin부터 bfs 시작
+        Queue<String> q = new ArrayDeque<>();
+        Set<String> visited = new HashSet<>();
+        int depth=0;
         
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for(int i=0; i<size; i++){
-                String cur = queue.remove();
-                if(cur.equals(target)){ //target을 찾으면 depth 리턴 후 종료
-                    return depth;
-                }
-                for(String word:words){
-                    if (canConvert(cur,word)&&!visited.containsKey(word)){
-                        queue.add(word);
-                        visited.put(word,true);
+        q.add(begin);
+        visited.add(begin);
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                String cur = q.remove();
+                if(cur.equals(target)) return depth;
+            for(String word:words){
+                if(canConvert(cur,word) && !visited.contains(word)){
+                    q.add(word);
+                    visited.add(word);
                     }
                 }
-            }
-            depth++;
+            }depth++;
         }
         return 0;
     }
     
-    // 현재 단어와 다음 비교 대상 단어의 알파벳 차이가 1 나면 true
     public boolean canConvert(String cur, String next){
-        int diff = 0;
+        int diff=0;
         for(int i=0; i<cur.length(); i++){
-            if (cur.charAt(i)!=next.charAt(i)){
+            if(cur.charAt(i)!=next.charAt(i)){
                 diff++;
             }
         }
-        if (diff==1){
-            return true;
-        }
-        return false;
+        return diff==1;
     }
 }
