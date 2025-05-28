b250aecd3006b17821c0544362c1a03a9200cc8d
Roa Kim <roa5108@gmail.com>
2025-05-28 22:47:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.86 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,39 +1,45 @@
 import java.util.*;
 
 class Solution {
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,-1,1};
     static int n, m;
     
     public int solution(int[][] maps) {
-        n = maps.length;
-        m = maps[0].length;
-        boolean[][] visited=new boolean[n][m];
+        n=maps.length;
+        m=maps[0].length;
+        boolean[][] visited = new boolean[n+1][m+1];
         
         bfs(0,0,visited,maps);
-        if(maps[n-1][m-1]<=1) return -1;
-        return maps[n-1][m-1];
+        if(maps[n-1][m-1]<=1) {
+            return -1;
+        }
+        else{
+            return maps[n-1][m-1];
+        }
     }
     
     public void bfs(int r, int c, boolean[][] visited, int[][] maps){
-        Queue<int[]> q = new LinkedList<>();
+        Queue<int[]> q = new ArrayDeque<>();
         q.add(new int[]{r,c});
         visited[r][c]=true;
         
         while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int curR = cur[0];
-            int curC = cur[1];
+            int[] cur=q.remove();
+            int cr=cur[0];
+            int cc=cur[1];
             
-            for(int i=0;i<4;i++){
-                int nextR = curR + dr[i];
-                int nextC = curC + dc[i];
-                if(0<=nextR && nextR<n && 0<=nextC && nextC<m && !visited[nextR][nextC] && maps[nextR][nextC]==1){
-                    visited[nextR][nextC]=true;
-                    maps[nextR][nextC]=maps[curR][curC]+1;
-                    q.add(new int[]{nextR,nextC});
-                }
+            for(int i=0; i<4; i++){
+            int nr=cr+dr[i];
+            int nc=cc+dc[i];
+            if(0<=nr && nr<n && 0<=nc && nc<m && !visited[nr][nc] && maps[nr][nc]==1){
+                q.add(new int[]{nr,nc});
+                visited[nr][nc]=true;
+                maps[nr][nc]=maps[cr][cc]+1;
             }
         }
+        }
+        
+        
     }
 }
