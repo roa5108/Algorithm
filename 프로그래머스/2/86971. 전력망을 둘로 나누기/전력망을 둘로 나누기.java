c5c08f1810455e5fa378363f40a48c2f7ea6d041
Roa Kim <roa5108@gmail.com>
2025-05-30 09:45:08 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.91 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -5,29 +5,29 @@ class Solution {
         int answer = Integer.MAX_VALUE;
         for(int i=0; i<wires.length; i++){
             List<List<Integer>> tree = new ArrayList<>();
-            for(int j=0; j<=n; j++){
-                tree.add(new ArrayList<>());
-            }
             
+            for(int j=0; j<=n; j++){
+                tree.add(new ArrayList<Integer>());
+            }
             for(int j=0; j<wires.length; j++){
-                if(i==j) continue;
+                if (i==j) continue;
                 tree.get(wires[j][0]).add(wires[j][1]);
                 tree.get(wires[j][1]).add(wires[j][0]);
             }
             boolean[] visited = new boolean[n+1];
-            int count = dfs(1, visited, tree);
+            int count = dfs(1,tree,visited);
             int diff = Math.abs(n-2*count);
-            answer=Math.min(diff, answer);
+            answer = Math.min(answer, diff);
         }
         return answer;
     }
     
-    public int dfs(int node, boolean[] visited, List<List<Integer>> tree){
+    public int dfs(int node, List<List<Integer>> tree, boolean[] visited){
         visited[node]=true;
-        int count=1;
-        for(int next : tree.get(node)){
+        int count = 1;
+        for(int next:tree.get(node)){
             if(!visited[next]){
-                count += dfs(next, visited, tree);
+                count+=dfs(next, tree, visited);
             }
         }
         return count;
