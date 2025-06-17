b3788ec526dfea71fde94bf29458b46d084f9df7
Roa Kim <roa5108@gmail.com>
2025-06-17 17:35:13 +0900

    [level 2] Title: 피로도, Time: 29.60 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[][] dungeons) {
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean[] visited = new boolean[dungeons.length];
+        recur(k,dungeons,new ArrayList<>(), ans, visited);
+        int maxCnt = 0;
+        for(List<Integer> sub : ans){
+            if(sub.size()>maxCnt){
+                maxCnt=sub.size();
+            }
+        }
+        return maxCnt;
+    }
+    
+    public void recur(int k, int[][] dungeons, List<Integer> curr, List<List<Integer>> ans, boolean[] visited){
+        ans.add(new ArrayList<>(curr));
+        for(int i=0; i<dungeons.length; i++){
+            if(k>=dungeons[i][0]){
+                if(!visited[i]){
+                    curr.add(i);
+                    visited[i]=true;
+                    recur(k-dungeons[i][1],dungeons,curr,ans,visited);
+                    curr.remove(curr.size()-1);
+                    visited[i]=false;
+                }
+            }
+        }
+    }
+}
