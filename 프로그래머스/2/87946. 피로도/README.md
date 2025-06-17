a88442da090f417357c17287b68d06543046aef6
Roa Kim <roa5108@gmail.com>
2025-06-17 23:43:44 +0900

    [level 2] Title: 피로도, Time: 19.52 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
 
 ### 성능 요약
 
-메모리: 79.8 MB, 시간: 29.60 ms
+메모리: 79.9 MB, 시간: 19.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 17일 17:35:10
+2025년 06월 17일 23:43:43
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -4,26 +4,24 @@ class Solution {
     public int solution(int k, int[][] dungeons) {
         List<List<Integer>> ans = new ArrayList<>();
         boolean[] visited = new boolean[dungeons.length];
-        recur(k,dungeons,new ArrayList<>(), ans, visited);
+        recur(k, dungeons, new ArrayList<>(), ans, visited);
         int maxCnt = 0;
-        for(List<Integer> sub : ans){
-            if(sub.size()>maxCnt){
-                maxCnt=sub.size();
-            }
+        for (List<Integer> sub : ans) {
+            maxCnt = Math.max(maxCnt, sub.size());
         }
         return maxCnt;
     }
-    
-    public void recur(int k, int[][] dungeons, List<Integer> curr, List<List<Integer>> ans, boolean[] visited){
+
+    public void recur(int k, int[][] dungeons, List<Integer> curr, List<List<Integer>> ans, boolean[] visited) {
         ans.add(new ArrayList<>(curr));
-        for(int i=0; i<dungeons.length; i++){
-            if(k>=dungeons[i][0]){
-                if(!visited[i]){
+        for (int i = 0; i < dungeons.length; i++) {
+            if (k >= dungeons[i][0]) {
+                if (!visited[i]) {
                     curr.add(i);
-                    visited[i]=true;
-                    recur(k-dungeons[i][1],dungeons,curr,ans,visited);
-                    curr.remove(curr.size()-1);
-                    visited[i]=false;
+                    visited[i] = true;
+                    recur(k - dungeons[i][1], dungeons, curr, ans, visited);
+                    curr.remove(curr.size() - 1);
+                    visited[i] = false;
                 }
             }
         }
