b48d3dacc7e5e8aa49c21476023622cd66e0b710
Roa Kim <roa5108@gmail.com>
2025-09-18 02:59:00 +0900

    [level 0] Title: 옹알이 (1), Time: 0.14 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -2,12 +2,11 @@ def solution(babbling):
     result=0
     arr = ["aya", "ye", "woo", "ma"]
     for b in babbling:
-        for i in range(4):
+        for i in range(4): # arr 길이만큼 반복해 모든 단어 startwith 검사
             for word in arr:
-                if word in b and b.startswith(word):
+                if word in b and b.startswith(word): # wyeoo에서 ye 삭제 시 woo가 남아 정상 단어 처리되는 경우 제외
                     b = b.replace(word, "")
-        print(b)
-        if b=="":
+        if b=="": # 모두 삭제되면 가능한 단어이므로 result+1
             result+=1
             
     return result
