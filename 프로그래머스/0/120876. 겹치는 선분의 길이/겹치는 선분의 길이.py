c2d7a889719c531c47f34f29d6840da14c916b8c
Roa Kim <roa5108@gmail.com>
2025-09-18 02:25:06 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.04 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,9 +1,9 @@
 def solution(lines):
-    cover=[0]*201 #-100~100 cover
+    cover=[0]*201 # -100~100 cover
     
     for a, b in lines:
         a, b = min(a,b), max(a,b)
         for x in range(a,b):
-            cover[x+100]+=1
+            cover[x+100]+=1 # +100으로 음수 index 방지
         
     return sum(1 for c in cover if c>=2)
