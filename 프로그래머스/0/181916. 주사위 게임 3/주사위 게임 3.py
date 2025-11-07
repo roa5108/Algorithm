a4c5173beda4d605b0795252629ce3d2655b1299
Roa Kim <roa5108@gmail.com>
2025-11-08 01:05:27 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.03 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+def solution(a, b, c, d):
+    counter = Counter([a,b,c,d])
+    key = list(counter.keys())
+    val = list(counter.values())
+    
+    if len(counter)==1:
+        return 1111*a
+    
+    if 3 in val:
+        p = [k for k,v in counter.items() if v == 3][0]
+        q = [k for k,v in counter.items() if v == 1][0]
+        return (10*p+q)**2
+    
+    if val.count(2)==2:
+        return (key[0]+key[1]) * abs(key[0]-key[1])
+    
+    if 2 in val and 1 in val:
+        others = [k for k,v in counter.items() if v==1]
+        return others[0]*others[1]
+    
+    if len(counter)==4:
+        return min(a,b,c,d)
