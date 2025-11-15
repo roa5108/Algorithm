c20f673d1af115544e92424c634ebbddf3738ccd
Roa Kim <roa5108@gmail.com>
2025-11-15 14:35:23 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,6 @@
 def solution(array, commands):
     answer = []
-    for i in commands:
-        arr=array[i[0]-1:i[1]]
-        arr.sort()
-        answer.append(arr[i[2]-1])
+    for c in commands:
+        arr=sorted(array[c[0]-1:c[1]])
+        answer.append(arr[c[2]-1])
     return answer
