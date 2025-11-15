246df2f3078ddf835f89912496162bb284d613c3
Roa Kim <roa5108@gmail.com>
2025-11-15 12:55:49 +0900

    [level 2] Title: 구명보트, Time: 8.89 ms, Memory: 9.62 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,12 @@
+def solution(people, limit):
+    people.sort()
+    i, j = 0, len(people)-1
+    cnt=0
+    
+    while i<=j:
+        if people[i]+people[j]<=limit:
+            i+=1
+        j-=1
+        cnt+=1
+    
+    return cnt
