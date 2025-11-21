62995fc45cbc7f2e242948079583958d14c7a5ba
Roa Kim <roa5108@gmail.com>
2025-11-21 20:59:20 +0900

    [level 2] Title: H-Index, Time: 0.28 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,9 +1,10 @@
 def solution(citations):
-    citations.sort()
+    citations.sort(reverse=True)
     arr=[]
+    print(citations)
     
     for idx, n in enumerate(citations):
-        if n>=len(citations)-idx:
-            arr.append(len(citations)-idx)
+        if n>=idx+1:
+            arr.append(idx+1)
             
     return max(arr) if arr else 0
