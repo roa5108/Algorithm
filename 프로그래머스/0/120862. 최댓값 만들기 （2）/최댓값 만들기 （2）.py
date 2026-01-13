7eb622fc959537e58dca1e112c05230ec9e1d518
Roa Kim <roa5108@gmail.com>
2026-01-13 14:16:01 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,6 +1,3 @@
 def solution(numbers):
     numbers.sort()
-    if numbers[0]<0 and numbers[1]<0:
-        return max(numbers[0]*numbers[1], numbers[-1]*numbers[-2])
-    else:
-        return numbers[-1]*numbers[-2]
+    return max(numbers[0]*numbers[1], numbers[-1]*numbers[-2])
