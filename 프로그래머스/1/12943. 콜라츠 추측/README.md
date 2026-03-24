0c58ac5682862755d9714d57587a4ea1997f9c9b
Roa Kim <roa5108@gmail.com>
2026-03-24 21:36:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.07 ms
+메모리: 82 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 24일 16:54:13
+2026년 03월 24일 21:36:38
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int num) {
+        long n = (long)num;
+        int answer = 0;
+        if (n==1) return 0;
+        
+        while (n!=1) {
+            if (n%2==0) {
+                n=n/2;
+            }
+            else {
+                n = n*3+1;
+            }
+            answer+=1;
+            
+            if (answer>=500) return -1;
+        }
+        return answer;
+    }
+}
